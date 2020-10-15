3a34bf963ff7fe46f539acbfc7dc039744b053e8
Emil Diaz <emilhdiaz@users.noreply.github.com>
2020-10-15 12:37:23 -0400

    Correcting sha256 hash for vulcan@v1.0.0-alpha.3.tar.gz

--- Formula/vulcan.rb
+++ Formula/vulcan.rb
@@ -4,7 +4,7 @@ class Vulcan < Formula
   desc "Like the roman god of metalworking and the forge, the vulcan CLI forges your development environment. "
   homepage "https://github.com/emilhdiaz/vulcan"
   url "https://github.com/emilhdiaz/vulcan/archive/v1.0.0-alpha.3.tar.gz"
-  sha256 "b5167b443c26423040215a86c95f11806de891b6fc1598d46cd21b96646af1fb"
+  sha256 "9b63702c17abb812062f10058ca6b3b85a1d180ea44260ea784cad17faf6c304"
   license "MIT"
 
   depends_on "yq"
