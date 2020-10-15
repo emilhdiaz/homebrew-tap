37560d880da16f96e6a65475620549c67aa8cfbe
Emil Diaz <emil.h.diaz@gmail.com>
2020-10-14 22:23:08 -0400

    Updating sha256 hash for vulcan

--- Formula/vulcan.rb
+++ Formula/vulcan.rb
@@ -4,7 +4,7 @@ class Vulcan < Formula
   desc "Like the roman god of metalworking and the forge, the vulcan CLI forges your development environment. "
   homepage "https://github.com/emilhdiaz/vulcan"
   url "https://github.com/emilhdiaz/vulcan/archive/v1.0.0-alpha.2.tar.gz"
-  sha256 ""
+  sha256 "a3f828dbcb44b0cd688cbcacf343974f5591794e35ea406e136af46d7f8fe6fb"
   license "MIT"
 
   depends_on "yq"
@@ -14,5 +14,4 @@ class Vulcan < Formula
     system "ls -la"
     prefix.install Dir["*"]
   end
-
 end
