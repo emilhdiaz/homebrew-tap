7754399d0f6ff9f08802bd96445d82746dfb7b54
Emil Diaz <emil.h.diaz@gmail.com>
2020-10-17 00:25:59 -0400

    Bumping vulcan to v1.0.0-alpha.4

--- Formula/vulcan.rb
+++ Formula/vulcan.rb
@@ -3,12 +3,14 @@
 class Vulcan < Formula
   desc "Like the roman god of metalworking and the forge, the vulcan CLI forges your development environment. "
   homepage "https://github.com/emilhdiaz/vulcan"
-  url "https://github.com/emilhdiaz/vulcan/archive/v1.0.0-alpha.3.tar.gz"
-  sha256 "9b63702c17abb812062f10058ca6b3b85a1d180ea44260ea784cad17faf6c304"
+  url "https://github.com/emilhdiaz/vulcan/archive/v1.0.0-alpha.4.tar.gz"
+  sha256 "3181a1e39d40985de830b85d21a65cfc5d9bd13b70543b1d50d15cda4d54c45d"
   license "MIT"
 
-  depends_on "yq"
   depends_on "coreutils"
+  depends_on "curl"
+  depends_on "yq"
+  depends_on "jq"
 
   def install
     prefix.install Dir["*"]
