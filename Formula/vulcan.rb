95e4633088f9aa5a1b6fb9f907101fad20e08077
Emil Diaz <emil.h.diaz@gmail.com>
2020-10-21 01:54:46 -0400

    Bumping vulcan to v1.0.0-alpha.6

--- Formula/vulcan.rb
+++ Formula/vulcan.rb
@@ -3,8 +3,8 @@
 class Vulcan < Formula
   desc "Like the roman god of metalworking and the forge, the vulcan CLI forges your development environment. "
   homepage "https://github.com/emilhdiaz/vulcan"
-  url "https://github.com/emilhdiaz/vulcan/archive/v1.0.0-alpha.5.tar.gz"
-  sha256 "edae7ab9ec3e16601fb4a0c2db9db7b769859183779259b02e6c18dc24e3b478"
+  url "https://github.com/emilhdiaz/vulcan/archive/v1.0.0-alpha.6.tar.gz"
+  sha256 "2c9f9c17d067aba129491416b3ef886971975f7e73932ddee0820e22371d8510"
   license "MIT"
 
   depends_on "coreutils"
